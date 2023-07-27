353652f5cf0bf7f48f798befa279dcd73e8ec756
yangwb1123 <1832227346@qq.com>
2023-07-27 16:23:32 +0800

    release: v0.0.2

--- go.mod
+++ go.mod
@@ -7,7 +7,14 @@ require (
 	github.com/gofrs/uuid v4.4.0+incompatible
 	github.com/golang-jwt/jwt/v5 v5.0.0
 	github.com/thanhpk/randstr v1.0.6
+	google.golang.org/grpc v1.57.0
 	google.golang.org/protobuf v1.31.0
 )
 
-require github.com/google/go-cmp v0.5.9 // indirect
+require (
+	github.com/golang/protobuf v1.5.3 // indirect
+	golang.org/x/net v0.9.0 // indirect
+	golang.org/x/sys v0.7.0 // indirect
+	golang.org/x/text v0.9.0 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20230525234030-28d5490b6b19 // indirect
+)
--- grpc/grpc.go
+++ grpc/grpc.go
@@ -1,9 +1,28 @@
 package grpc
 
 import (
+	"context"
 	"encoding/json"
+	"net"
+	"time"
 
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/credentials/insecure"
+	"google.golang.org/grpc/keepalive"
+	"google.golang.org/grpc/metadata"
+	"google.golang.org/grpc/status"
 	"google.golang.org/protobuf/types/known/structpb"
+)
+
+type CommonResp struct {
+	Result *structpb.Struct
+
+	GetResult func() *structpb.Struct
+}
+
+const (
+	AuthTokenKey string = "authorization"
 )
 
 func ToRpcStruct(data any) *structpb.Struct {
@@ -23,4 +42,76 @@ func ToRpcStruct(data any) *structpb.Struct {
 	}
 
 	return s
+}
+
+func RunServer(addr string, register func(grpcServer *grpc.Server), auth func(token string) bool) error {
+	listener, err := net.Listen("tcp", addr)
+	if err != nil {
+		return err
+	}
+
+	grpcServer := grpc.NewServer(
+		grpc.KeepaliveParams(keepalive.ServerParameters{
+			MaxConnectionIdle: 10 * time.Second,
+		}),
+		grpc.UnaryInterceptor(func(ctx context.Context,
+			req interface{},
+			info *grpc.UnaryServerInfo,
+			handler grpc.UnaryHandler) (resp interface{}, err error) {
+
+			md, ok := metadata.FromIncomingContext(ctx)
+			authToken := md.Get(AuthTokenKey)
+			if !ok || (len(authToken) < 1) || (len(authToken) > 0 && auth(authToken[0])) {
+				return resp, status.Error(codes.Unauthenticated, "unauthenticated")
+			}
+
+			return handler(ctx, req)
+		}),
+	)
+
+	register(grpcServer)
+
+	return grpcServer.Serve(listener)
+}
+
+func NewClient(addr string, authToken string) (*grpc.ClientConn, error) {
+	conn, err := grpc.Dial(addr,
+		grpc.WithTransportCredentials(insecure.NewCredentials()),
+		grpc.WithUnaryInterceptor(
+			func(ctx context.Context,
+				method string,
+				req,
+				reply interface{},
+				cc *grpc.ClientConn,
+				invoker grpc.UnaryInvoker,
+				opts ...grpc.CallOption) error {
+
+				if authToken != "" {
+					md := metadata.New(map[string]string{
+						AuthTokenKey: authToken,
+					})
+
+					ctx = metadata.NewOutgoingContext(ctx, md)
+				}
+
+				err := invoker(ctx, method, req, reply, cc, opts...)
+
+				if err != nil {
+					return err
+				}
+
+				if resp, ok := reply.(*CommonResp); ok {
+					resp.Result = ToRpcStruct(map[string]any{
+						"data": reply.(*CommonResp).GetResult().AsMap()["data"],
+					})
+				}
+
+				return nil
+			}))
+
+	if err != nil {
+		return nil, err
+	}
+
+	return conn, nil
 }
